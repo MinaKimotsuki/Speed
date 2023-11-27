f151ddee689a33a1d93d8357bfab4a638846f558
mina15 <miina.kimotsuki@gmail.com>
2023-11-27 22:15:39 +0900

    カードの番号の仕分け・カードを場に出す条件

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     GameObject clickedGameObject;
     bool isCardMovingToHand = false;
     Vector3 screenPoint;
+    int orderInRayer = 0;
+    [SerializeField] PlayerCardsController playerCardsController;
     [SerializeField] GameObject card;
     [SerializeField] GameObject cards;
     [SerializeField] GameObject hand1;
     [SerializeField] GameObject hand2;
     [SerializeField] GameObject hand3;
     [SerializeField] GameObject hand4;
+    bool isHand1Full = false;
+    bool isHand2Full = false;
+    bool isHand3Full = false;
+    bool isHand4Full = false;
 
     // Start is called before the first frame update
     void Start()
@@ -37,10 +44,19 @@ public class PlayerController : MonoBehaviour
             RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
             if (hit2d)
             {
-                clickedGameObject = Instantiate(card, cards.transform.position, Quaternion.identity);
-                if (hit2d.collider.gameObject.CompareTag("Cards"))
+                if (isHand1Full == false || isHand2Full == false || isHand3Full == false || isHand4Full == false)
                 {
-                    isCardMovingToHand = true;
+                    if (hit2d.collider.gameObject.CompareTag("Cards"))
+                    {
+                        clickedGameObject = Instantiate(card, cards.transform.position, Quaternion.identity);
+                        clickedGameObject.GetComponent<SpriteRenderer>().sortingOrder = orderInRayer;
+                        clickedGameObject.transform.GetChild(0).GetComponent<MeshRenderer>().sortingOrder = orderInRayer;
+                        clickedGameObject.transform.GetChild(1).GetComponent<MeshRenderer>().sortingOrder = orderInRayer;
+                        orderInRayer++;
+                        clickedGameObject.transform.GetChild(0).GetComponent<TextMeshPro>().text = playerCardsController.PlayerCards[0][1].ToString();
+                        clickedGameObject.transform.GetChild(1).GetComponent<TextMeshPro>().text = playerCardsController.PlayerCards[0][0].ToString();
+                        isCardMovingToHand = true;
+                    }
                 }
             }
         }
@@ -50,6 +66,7 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetMouseButtonUp(0))
         {
+            if (clickedGameObject == null) return;
             GameObject pastClickedGameObject = clickedGameObject;
             clickedGameObject = null;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -64,8 +81,71 @@ public class PlayerController : MonoBehaviour
                 clickedGameObject = hit2d.transform.gameObject;
                 if (clickedGameObject.CompareTag("Hand"))
                 {
+                    if (clickedGameObject.name == "Hand1")
+                    {
+                        if (isHand1Full == false)
+                        {
+                            playerCardsController.PlayerCards.RemoveAt(0);
+                            isCardMovingToHand = false;
+                            pastClickedGameObject.transform.position = clickedGameObject.transform.position;
+                            isHand1Full = true;
+                        }
+                        else
+                        {
+                            Destroy(pastClickedGameObject);
+                            isCardMovingToHand = false;
+                        }
+                    }
+                    if (clickedGameObject.name == "Hand2")
+                    {
+                        if (isHand2Full == false)
+                        {
+                            playerCardsController.PlayerCards.RemoveAt(0);
+                            isCardMovingToHand = false;
+                            pastClickedGameObject.transform.position = clickedGameObject.transform.position;
+                            isHand2Full = true;
+                        }
+                        else
+                        {
+                            Destroy(pastClickedGameObject);
+                            isCardMovingToHand = false;
+                        }
+                    }
+                    if (clickedGameObject.name == "Hand3")
+                    {
+                        if (isHand3Full == false)
+                        {
+                            playerCardsController.PlayerCards.RemoveAt(0);
+                            isCardMovingToHand = false;
+                            pastClickedGameObject.transform.position = clickedGameObject.transform.position;
+                            isHand3Full = true;
+                        }
+                        else
+                        {
+                            Destroy(pastClickedGameObject);
+                            isCardMovingToHand = false;
+                        }
+                    }
+                    if (clickedGameObject.name == "Hand4")
+                    {
+                        if (isHand4Full == false)
+                        {
+                            playerCardsController.PlayerCards.RemoveAt(0);
+                            isCardMovingToHand = false;
+                            pastClickedGameObject.transform.position = clickedGameObject.transform.position;
+                            isHand4Full = true;
+                        }
+                        else
+                        {
+                            Destroy(pastClickedGameObject);
+                            isCardMovingToHand = false;
+                        }
+                    }
+                }
+                else
+                {
+                    Destroy(pastClickedGameObject);
                     isCardMovingToHand = false;
-                    pastClickedGameObject.transform.position = clickedGameObject.transform.position;
                 }
             }
         }
